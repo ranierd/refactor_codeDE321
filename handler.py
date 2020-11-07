0d34a7d5e879c0d48dab2c550bce2a8d6fd52e59
ranierd <41879257+ranierd@users.noreply.github.com>
2020-11-07 22:07:53 +1300

    handler.py

---
+++ handler.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from typing import Optional
+from abc import ABC, abstractmethod
+
+
+class Handler(ABC):
+    """
+    The Handler interface declares a method for building the chain of handlers.
+    It also declares a method for executing a request.
+    """
+
+    @abstractmethod
+    def set_next(self, handler: Handler) -> Handler:
+        pass
+
+    @abstractmethod
+    def handle(self, request) -> Optional[str]:
+        pass
