c5e8bc09d960b586c2010acda4d53429bf518635
ranierd <41879257+ranierd@users.noreply.github.com>
2020-11-09 16:04:15 +1300

    Update js_convert.py

--- js_convert.py
+++ js_convert.py
@@ -15,49 +15,34 @@ class JSConvert:
             print(e)
 
     def get_functions(self, data: []):
-        bracket_count = 0
-        i = 0
         functions = []
-        in_function = False
         try:
             for line in data:
-                result = search("class .* {", line)
-                if result:
-                    r = findall('{', line)
-                    if len(r) > 0:
-                        bracket_count += len(r)
-                    r = findall('}', line)
-                    if r:
-                        if bracket_count > 0:
-                            bracket_count -= len(r)
-                        if bracket_count == 0:
-                            in_function = False
-                            continue
-                elif not in_function:
-                    result = search(r'\S(.*).*{', line)
-                    if result is not None:
-                        current_level = findall(r'(\S+)[(].*[)].*{', line)
-                        print(current_level)
+                current_level = findall(r'(\S+)[(].*[)].*{', line)
+                if current_level:
+                    functions.append(current_level[0] + "()")
+            return functions
+        except (AssertionError, TypeError, AttributeError, IndexError) as e:
+            print(e)
 
+    def get_attributes(self, data: []):
+        attributes = []
+        try:
+            for line in data:
+                attribute = findall(r'this\.(\S+)', line)
+                if attribute:
+                    attributes.append(attribute)
+            print(attributes)
         except (AssertionError, TypeError, AttributeError) as e:
             print(e)
 
-    def get_attributes(self, functions: [], data: []):
-        i = 0
-        for line in data:
-            try:
-                if functions[i]:
-                    return self.attributes.append(findall(r'this\.(\S+)', line))
-            except (AssertionError, TypeError, AttributeError) as e:
-                print(e)
-
-    #def formatted(self, classes: [], functions: [], attributes []):
 
 
 if __name__ == '__main__':
-    src_code = open("C:\\Users\\Ranier\\Downloads\\python-assignment-master\\resources\\16_game.js")
+    src_code = open("H:\\Ranier and Daniels python-assignment-2\\resources\\16_game.js")
     all_lines = src_code.readlines()
     js = JSConvert
-    noob = js.get_classes(all_lines, all_lines)
+    js.get_classes(all_lines, all_lines)
     js.get_functions(all_lines, all_lines)
+    js.get_attributes(all_lines,all_lines)
     src_code.close()
